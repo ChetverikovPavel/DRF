5347e3f126c6aa8a3e5c25fc2f297d2e83a4278f
Chetverikov Pavel <82161162+ChetverikovPavel@users.noreply.github.com>
2021-12-27 16:32:58 +0300

    Lesson 1 (#1)
    * lesson_1. usersapp added. DRF enabled.
    
    * lesson_1. user model added. API done.
    
    * lesson_1. Code style fix.

---
+++ usersapp/models.py
@@ -0,0 +1,14 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    uuid = models.UUIDField(primary_key=True, default=uuid4())
+    email = models.EmailField(unique=True, blank=False)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday = models.DateField(blank=True, null=True)
+    
