876f32ac99e8d11b28db0dddcc27283b390528d0
Chetverikov Pavel <82161162+ChetverikovPavel@users.noreply.github.com>
2022-01-20 01:59:35 +0300

    Lesson 2 (#2)
    * lesson_2. Create React app 'frontend'.
    
    * lesson_2. CORS settings set.
    
    * lesson_2. Create frontend.

--- TODO/settings.py
+++ TODO/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "usersapp",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "usersapp.User"
@@ -46,6 +47,7 @@ AUTH_USER_MODEL = "usersapp.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     # 'django.middleware.csrf.CsrfViewMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
