f28d32b38b43361d13f0db48fd2d5fe713b8999e
Pavel <kas62-96@mail.ru>
2022-01-20 01:58:18 +0300

    lesson_4. Views update. Filters add. Paginations add. Code style fix

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -1,14 +1,17 @@
 from dataclasses import field
+
 from rest_framework.serializers import ModelSerializer
+
 from .models import Project, ToDo
 
 
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ToDoSerializer(ModelSerializer):
     class Meta:
         model = ToDo
-        exclude = ('is_active',)
+        exclude = ("is_active",)
