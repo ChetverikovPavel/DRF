90b2ea963fecdff5ec33c3496d1bf690624e2ccc
Chetverikov Pavel <82161162+ChetverikovPavel@users.noreply.github.com>
2022-02-07 15:39:23 +0300

    lesson_5. Routing added. (#14)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import UserList from './components/Users.js'
+import ProjectList from './components/Projects';
+import ToDoList from './components/ToDos';
 import axios from 'axios'
+import {HashRouter, Link, Route, Routes} from "react-router-dom"
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'users': []
+      'users': [],
+      'projects': [],
+      'todos': []
     }
   }
 
@@ -18,16 +23,55 @@ class App extends React.Component {
         const users = response.data
           this.setState(
           {
-            'users': users
+            'users': users.results
           }
         )
       }).catch(error => console.log(error))
+
+    axios.get('http://127.0.0.1:8000/api/projects')
+    .then(response => {
+      const projects = response.data
+        this.setState(
+        {
+          'projects': projects.results
+        }
+      )
+    }).catch(error => console.log(error))
+
+    axios.get('http://127.0.0.1:8000/api/todos')
+    .then(response => {
+      const todos = response.data
+        this.setState(
+        {
+          'todos': todos.results
+        }
+      )
+    }).catch(error => console.log(error))
   }
   
   render() {
     return (
-      <div>
-        <UserList users={this.state.users} />
+      <div class='app'>
+        <HashRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Users</Link>
+              </li>
+              <li>
+                <Link to='/projects'>Projects</Link>
+              </li>
+              <li>
+                <Link to='/todos'>ToDos</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/' element={<UserList users={this.state.users} />}/>
+            <Route path='/projects' element={<ProjectList projects={this.state.projects} />}/>
+            <Route path='/todos' element={<ToDoList todos={this.state.todos} />}/>            
+          </Routes>
+        </HashRouter>
       </div>
     )
   }
